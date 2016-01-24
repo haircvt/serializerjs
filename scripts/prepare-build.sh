a3057c9c22864748eed737df5ecd4ad9b0db2b4d
Théo FIDRY <theo.fidry@gmail.com>
2016-01-24 11:45:37 +0000

    Configure Travis for deployment

--- scripts/prepare-build.sh
+++ scripts/prepare-build.sh
@@ -1,1 +1,30 @@
 #!/usr/bin/env bash
+#
+# Functions for publishing and deploying artifacts
+#
+
+setupDeployment() {
+  isDeployBuild
+  if [[ 0 = "$?" ]]; then
+    log "No deployment scheduled for this build"
+
+    return 0;
+  fi
+
+  log "Setting up deployment configuration";
+
+  log "Setting Git user configuration";
+  if [[ -z "$GITHUB_USER_NAME" ]] || [[ -z "$GITHUB_USER_EMAIL" ]]; then
+    log "Could not configure Git user" --error
+  else
+    echo "    user: $GITHUB_USER_NAME"
+    echo "    temail: $GITHUB_USER_EMAIL"
+
+    git config --global user.name "$GITHUB_USER_NAME"
+    git config --global user.email "$GITHUB_USER_EMAIL"
+  fi
+
+  log "Done" --success
+}
+
+export -f setupDeployment
